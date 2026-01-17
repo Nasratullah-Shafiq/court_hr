1c858894ce8f71edb79942d79a537fce0cccc27c
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-01-17 13:01:54 +0430

    init file added

---
+++ models/__init__.py
@@ -0,0 +1,22 @@
+from . import court_hr_employee
+from . import court_hr_publication
+from . import court_hr_experience
+from . import court_hr_health
+from . import court_hr_travel
+from . import court_hr_property
+from . import court_hr_education
+from . import court_hr_relatives
+from . import court_hr_district
+from . import court_hr_language
+from . import court_hr_exam
+from . import court_hr_training
+from . import court_hr_punishment
+from . import court_hr_reward
+from . import court_hr_appraisal
+from . import court_hr_retirement
+from . import court_hr_guarantee
+from . import court_hr_document_attachment
+from . import court_hr_fire
+
+
+
