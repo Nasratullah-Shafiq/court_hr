590bfc1d7602d772640a8a6f2d4758198f18132e
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-01-29 09:34:39 +0430

    new changes

--- models/court_hr_punishment.py
+++ models/court_hr_punishment.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models, api
-
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+from datetime import date, datetime, timedelta
+import datetime
 
 class HrEmployeeInherit(models.Model):
     _inherit = 'hr.employee'
@@ -14,6 +16,7 @@ class HrEmployeeInherit(models.Model):
 class EmployeePunishment(models.Model):
     _name = 'employee.punishment'
     _description = 'Employee punishment'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     employee_id = fields.Many2one('hr.employee', string='Employee')
 
@@ -46,6 +49,38 @@ class EmployeePunishment(models.Model):
     punishment_date = fields.Date(string='Date Of Punishment')
 
     punishment_remarks = fields.Text(string='Remarks')
+
+    punishment_month = fields.Selection(
+        [
+            ('1', 'January'), ('2', 'February'), ('3', 'March'), ('4', 'April'),
+            ('5', 'May'), ('6', 'June'), ('7', 'July'), ('8', 'August'),
+            ('9', 'September'), ('10', 'October'), ('11', 'November'), ('12', 'December')
+        ],
+        string="Punishment Month",
+        compute="_compute_punishment_year_month",
+        store=True
+    )
+
+    punishment_year = fields.Selection(
+        selection=lambda self: [
+            (str(y), str(y)) for y in
+            range(datetime.date.today().year - 30,
+                  datetime.date.today().year + 10)
+        ],
+        string="Punishment Year",
+        compute="_compute_punishment_year_month",
+        store=True
+    )
+
+    @api.depends('punishment_end_date')
+    def _compute_punishment_year_month(self):
+        for rec in self:
+            if rec.punishment_end_date:
+                rec.punishment_month = str(rec.punishment_end_date.month)
+                rec.punishment_year = str(rec.punishment_end_date.year)
+            else:
+                rec.punishment_month = False
+                rec.punishment_year = False
 
 
 
