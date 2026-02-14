520b8fc777e62ec33cba0f9ddf4c235acb8c46b3
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-02-14 07:05:56 +0430

    new changes

--- models/court_hr_language.py
+++ models/court_hr_language.py
@@ -13,26 +13,43 @@ class EgpHrInherit(models.Model):
 
 class EmployeeLanguage(models.Model):
     _name = 'employee.language'
-    # _description = 'Language'
+    _description = 'Language'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     employee_id = fields.Many2one('hr.employee', string='Employee')
-    language = fields.Char(string='Language', required=True)
-    reading = fields.Selection([('good', 'Good'), ('َvery_good', 'Very Good'),
-                                ('excellent', 'Excellent'), ], string="Reading", default='excellent')
-    speaking = fields.Selection([('good', 'Good'), ('َvery_good', 'Very Good'),
-                                 ('excellent', 'Excellent'), ], string="Speaking", default='excellent')
-    listening = fields.Selection([('good', 'Good'), ('َvery_good', 'Very Good'),
-                                  ('excellent', 'Excellent'), ], string="Listening", default='excellent')
-    writing = fields.Selection([('good', 'Good'), ('َvery_good', 'Very Good'),
-                                ('excellent', 'Excellent'), ], string="Writing", default='excellent')
 
-    @api.constrains('language')
-    def _check_language(self):
-        for record in self:
-            if record.language and not re.match('^[A-Za-z]+$', record.language):
-                raise ValidationError("The Language field should only contain alphabetic characters.")
+    language_id = fields.Many2one('employee.language.master', string='Language', required=True, tracking=True)
 
+    LANGUAGE_LEVEL = [
+        ('a1', 'A1 (Beginner)'),
+        ('a2', 'A2 (Elementary)'),
+        ('b1', 'B1 (Intermediate)'),
+        ('b2', 'B2 (Upper-Intermediate)'),
+        ('c1', 'C1 (Advanced)'),
+        ('c2', 'C2 (Proficient)'),
+        ('native', 'Native')
+    ]
 
+    # Language skill fields
+    reading = fields.Selection(LANGUAGE_LEVEL, string="Reading", default='b2', tracking=True)
+    speaking = fields.Selection(LANGUAGE_LEVEL, string="Speaking", default='b2', tracking=True)
+    listening = fields.Selection(LANGUAGE_LEVEL, string="Listening", default='b2', tracking=True)
+    writing = fields.Selection(LANGUAGE_LEVEL, string="Writing", default='b2', tracking=True)
+
+    remarks = fields.Text(string="Remarks", tracking=True)
+
+    attachments = fields.Many2many('ir.attachment', string="Attachments")
+
+    class EmployeeLanguageMaster(models.Model):
+        _name = 'employee.language.master'
+        _description = 'Languages Master'
+
+        name = fields.Char(string='Language', required=True, unique=True, translate=True)
+        code = fields.Char(string='Code')  # Optional, e.g., EN, FR, PS, DA
+
+        _sql_constraints = [
+            ('unique_language', 'unique(name)', 'Language must be unique!')
+        ]
 
 
 
